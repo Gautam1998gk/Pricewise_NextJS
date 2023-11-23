0ffd44658205c23e3e2b817cc66e43fda88a4938
Gautam1998gk <gowtham1998gk@gmail.com>
2023-11-23 13:54:11 +0530

    main

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -11,6 +11,11 @@ import {
 import { generateEmailBody, sendEmail } from "@/lib/nodeMailer";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300
+export const dynamic = "force-dynamic"
+export const revalidate = 0
+
+
 export async function GET() {
   try {
     connectToDB();
